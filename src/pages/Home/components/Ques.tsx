cd4cb026d07abfeaa7696b8f426543fbc2e83516
dailyFunctions <danaop652@gmail.com>
2025-04-14 09:23:27 +0300

    fixed styling direction

--- src/pages/Home/components/Ques.tsx
+++ src/pages/Home/components/Ques.tsx
@@ -1,6 +1,6 @@
-import '../../../style/Home/section.ques.css';
-import { assets } from '../../../assets/assets';
-import Image from '../../../components/ImageComponent/Image';
+import "../../../style/Home/section.ques.css";
+import { assets } from "../../../assets/assets";
+import Image from "../../../components/ImageComponent/Image";
 
 // translate to english
 
@@ -13,40 +13,54 @@ const Ques = () => {
           <span>Maybe some questions are common for you</span>
         </div>
         <div className="section-ques-list">
-          <div className="section-ques-box" style={{cursor: "pointer"}}>
+          <div className="section-ques-box" style={{ cursor: "pointer" }}>
             <div className="section-ques-box-text">
-              <div className="plus-icon" style={{cursor: "pointer"}}>
-                <Image src={assets.plus} className="text-yellow-600" alt="plus" />
+              <div className="plus-icon" style={{ cursor: "pointer" }}>
+                <Image
+                  src={assets.plus}
+                  className="text-yellow-600"
+                  alt="plus"
+                />
               </div>
               <span>Can I download movies from Filimo?</span>
             </div>
           </div>
-            <div className="section-ques-box" style={{cursor: "pointer"}}>
+          <div className="section-ques-box" style={{ cursor: "pointer" }}>
             <div className="section-ques-box-text">
               <div className="plus-icon">
-                <Image src={assets.plus} className="text-yellow-600" alt="plus" />
+                <Image
+                  src={assets.plus}
+                  className="text-yellow-600"
+                  alt="plus"
+                />
               </div>
               <span>Is Filimo available outside of Iran?</span>
             </div>
           </div>
-          <div className="section-ques-box" style={{cursor: "pointer"}}>
+          <div className="section-ques-box" style={{ cursor: "pointer" }}>
             <div className="section-ques-box-text">
               <div className="plus-icon">
-                <Image src={assets.plus} className="text-yellow-600" alt="plus" />
+                <Image
+                  src={assets.plus}
+                  className="text-yellow-600"
+                  alt="plus"
+                />
               </div>
               <span>
                 Do I need to buy a subscription to watch online movies?
               </span>
             </div>
           </div>
-          <div className="section-ques-box" style={{cursor: "pointer"}}>
+          <div className="section-ques-box" style={{ cursor: "pointer" }}>
             <div className="section-ques-box-text">
               <div className="plus-icon ">
-                <Image src={assets.plus} className="text-yellow-600" alt="plus" />
+                <Image
+                  src={assets.plus}
+                  className="text-yellow-600"
+                  alt="plus"
+                />
               </div>
-              <span>
-                How can I download the Android or iOS Filimo app?
-              </span>
+              <span>How can I download the Android or iOS Filimo app?</span>
             </div>
           </div>
         </div>
