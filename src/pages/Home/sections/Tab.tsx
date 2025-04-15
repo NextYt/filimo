4efaa5f10d976cbb9f794e8badeb4a2786fafcd3
dailyFunctions <danaop652@gmail.com>
2025-04-15 23:39:41 +0300

    some style fixes

--- src/pages/Home/sections/Tab.tsx
+++ src/pages/Home/sections/Tab.tsx
@@ -1,13 +1,13 @@
-import '../../../style/Home/section-tab.css';
-import { assets } from '../../../assets/assets';
-import Button from '../../../components/Button/Button';
-import Image from '../../../components/ImageComponent/Image';
-import { useSectionsSelector } from '../../../context';
+import "../../../style/Home/section-tab.css";
+import { assets } from "../../../assets/assets";
+import Button from "../../../components/Button/Button";
+import Image from "../../../components/ImageComponent/Image";
+import { useSectionsSelector } from "../../../context";
 
 const Tab = () => {
   // Using our sections context instead of direct import
-  const tabSection = useSectionsSelector(context => context.state.tabSection);
-  
+  const tabSection = useSectionsSelector((context) => context.state.tabSection);
+
   const { buttonText, features } = tabSection;
 
   return (
@@ -20,11 +20,13 @@ const Tab = () => {
         <div className="section-detail-list section-tab-list">
           <div className="section-tab-inner-dev">
             {features.map((feature, index) => (
-              <div key={index} className="section-detail-list-item tab-text">
-                <Image src={assets.checkmark} className="text-green-500 text-xl" alt="checkmark" />
-                <span className="detail-tab-item-text">
-                  {feature.text}
-                </span>
+              <div key={`feature-${feature.text}-${index}`} className="tab-text">
+                <Image
+                  src={assets.checkmark}
+                  className="text-green-500 text-2xl"
+                  alt="checkmark"
+                />
+                <span className="detail-tab-item-text">{feature.text}</span>
               </div>
             ))}
           </div>
